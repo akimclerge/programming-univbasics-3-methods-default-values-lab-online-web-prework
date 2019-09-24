b497c2be5f295533277747159c6ed66924ab7a8c
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-24 21:39:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
-def meal_choice (veg1 = "broccoli" , veg2 = "macaroni", protien = "meat")
+def meal_choice = (veg1 = "broccoli" , veg2 = "macaroni", protien = "meat")
   p "A plate of #{protien} with #{veg1} and #{veg2}."
 end
-
+def 
 p "what a nutritious meal!"
 end
 
