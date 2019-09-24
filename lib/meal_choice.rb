aeadb41e1ff09605529eb8d421d35760336baffb
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-24 21:58:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 
-def meal_choice (veg1 = "broccoli" , veg2 = "macaroni", protien = "meat", "tofu")
+def meal_choice (veg1 = "broccoli" , veg2 = "macaroni", protien = "meat",)
   p "A plate of #{protien} with #{veg1} and #{veg2}."
 end
 
